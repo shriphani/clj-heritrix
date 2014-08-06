429ad9e07e70168cbc3aae3625e4430c86825109
Shriphani Palakodety <shriphanip@gmail.com>
2014-08-06 13:59:51 -0400

    two implementations left

--- src/clj_heritrix/core.clj
+++ src/clj_heritrix/core.clj
@@ -3,12 +3,84 @@
    Heritrix rest api"
   (:require [clj-http.client :as client]))
 
-(defn create
-  [engine-loc job-name user pass]
-  (client/post engine-loc
+(defn send-action
+  [end-point action form-params user pass]
+  (client/post end-point
                {:digest-auth [user pass]
-                :form-params {:createpath job-name
-                              :action "create"}
+                :form-params (merge form-params
+                                    {:action action})
                 :insecure? true
                 :throw-entire-message? true}))
+
+(defn create
+  [engine-loc job-name user pass]
+  (send-action engine-loc
+               "create"
+               {:createpath job-name}
+               user
+               pass))
+
+(defn add-job-directory
+  [engine-loc path user pass]
+  (send-action engine-loc
+               "add"
+               {:addpath path}
+               user
+               pass))
+
+(defn build-job-configuration
+  [job-loc user pass]
+  (send-action job-loc
+               "build"
+               nil
+               user
+               pass))
+
+(defn launch-job-configuration
+  [job-loc user pass]
+  (send-action job-loc
+               "launch"
+               nil
+               user
+               pass))
+
+(defn rescan-job-directory
+  [engine-loc user pass]
+  (send-action engine-loc
+               "rescan"
+               nil
+               user
+               pass))
+
+(defn pause-job
+  [job-loc user pass]
+  (send-action job-loc
+               "pause"
+               nil
+               user
+               pass))
+
+(defn unpause-job
+  [job-loc user pass]
+  (send-action job-loc
+               "unpause"
+               nil
+               user
+               pass))
+
+(defn terminate-job
+  [job-loc user pass]
+  (send-action job-loc
+               "terminate"
+               nil
+               user
+               pass))
+
+(defn teardown-job
+  [job-loc user pass]
+  (send-action job-loc
+               "teardown"
+               nil
+               user
+               pass))
 
