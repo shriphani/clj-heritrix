b90b79cf7fa4db40b7d3fb4356173be2ea1068e8
Shriphani Palakodety <shriphanip@gmail.com>
2014-08-05 22:07:15 -0400

    added create call

--- src/clj_heritrix/core.clj
+++ src/clj_heritrix/core.clj
@@ -1,6 +1,14 @@
-(ns clj-heritrix.core)
+(ns clj-heritrix.core
+  "Clojure implementation of the
+   Heritrix rest api"
+  (:require [clj-http.client :as client]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn create
+  [engine-loc job-name user pass]
+  (client/post engine-loc
+               {:digest-auth [user pass]
+                :form-params {:createpath job-name
+                              :action "create"}
+                :insecure? true
+                :throw-entire-message? true}))
+
