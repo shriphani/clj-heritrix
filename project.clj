fce1b161c81f0a176d1933fc72d27c5981e973ce
Shriphani Palakodety <shriphanip@gmail.com>
2014-08-09 00:36:17 -0400

    version bump

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj_heritrix "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj_heritrix "0.1.0"
+  :description "Clojure implementation of the Heritrix API"
+  :url "http://github.com/shriphani/clj-heritrix/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-http "0.9.2"]
